4917495270d281f1b2e7661a611643e8cad98f87
Frankie Bruno <frankiebruno1991@gmail.com>
2023-09-19 14:22:29 -0500

    almost

--- src/managers/AuthManager.js
+++ src/managers/AuthManager.js
@@ -1,5 +1,5 @@
 export const loginUser = (user) => {
-  return fetch("http://localhost:8000/login", {
+  return fetch("https://6509f43e18794a397f56ccd6--honeyrae.netlify.app/login", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -11,7 +11,7 @@ export const loginUser = (user) => {
 }
 
 export const registerUser = (user) => {
-  return fetch("http://127.0.0.1:8000/register", {
+  return fetch("https://6509f43e18794a397f56ccd6--honeyrae.netlify.app/register", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
--- src/managers/CustomerManager.js
+++ src/managers/CustomerManager.js
@@ -1,7 +1,7 @@
 import { getToken } from "../utils/getToken"
 
 export const getAllCustomers = () => {
-  return fetch("http://localhost:8000/customers", {
+  return fetch("https://6509f43e18794a397f56ccd6--honeyrae.netlify.app/customers", {
     headers: {
       Authorization: `Token ${getToken()}`
     }
